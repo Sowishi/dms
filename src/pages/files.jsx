a48960f8f8736de903a88233c370b8fc826c182a
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2023-11-25 00:33:35 +0800

    completed the files page

--- src/pages/files.jsx
+++ src/pages/files.jsx
@@ -1,9 +1,330 @@
+import { FaEye, FaFile, FaFolder } from "react-icons/fa";
 import Layout from "../layout/layout";
+import { Breadcrumb, Button, Dropdown, Modal } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Form } from "react-router-dom";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import { auth, db, storage } from "../../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const Files = () => {
+  const [storages, setStorages] = useState([]);
+  const [currentFolder, setCurrentFolder] = useState("files");
+
+  const fetchData = () => {
+    setCurrentFolder("files");
+    const q = query(
+      collection(db, "storage", auth.currentUser.uid, "files"),
+      orderBy("createdAt", "desc")
+    );
+    onSnapshot(q, (snapshot) => {
+      const storages = [];
+      snapshot.docs.forEach((doc) => {
+        const data = { ...doc.data(), id: doc.id };
+        storages.push(data);
+      });
+      setStorages(storages);
+    });
+  };
+
+  const fetchFolder = (folderName) => {
+    const q = query(
+      collection(db, "storage", auth.currentUser.uid, folderName),
+      orderBy("createdAt", "desc")
+    );
+    onSnapshot(q, (snapshot) => {
+      const storages = [];
+      snapshot.docs.forEach((doc) => {
+        const data = { ...doc.data(), id: doc.id };
+        storages.push(data);
+      });
+      setStorages(storages);
+    });
+  };
+
+  function AddFolder(props) {
+    const [show, setShow] = useState(false);
+    const [folders, setFolders] = useState();
+    const [folderName, setFolderName] = useState(null);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const createFolder = () => {
+      const data = {
+        owner: auth.currentUser.uid,
+        isFolder: true,
+        name: folderName,
+        createdAt: serverTimestamp(),
+      };
+      addDoc(collection(db, "storage", auth.currentUser.uid, "files"), data);
+    };
+
+    return (
+      <>
+        <Button variant="primary " onClick={handleShow}>
+          <h6 className="fw-bold text-white px-3 mb-0 py-1">Add Folder</h6>
+        </Button>{" "}
+        <Modal size="lg" show={show} onHide={handleClose}>
+          <Modal.Header className="bg-primary" closeButton>
+            <Modal.Title>Folder Name</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <input
+              onChange={(e) => setFolderName(e.target.value)}
+              type="text"
+              className="form-control"
+              placeholder="Folder Name"
+            />{" "}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={createFolder}>Add Folder</Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+
+  function AddFile(props) {
+    const [show, setShow] = useState(false);
+    const [file, setFile] = useState(null);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const createFile = () => {
+      if (file) {
+        const storageRef = ref(storage, `uploads/${file.name}`);
+        uploadBytes(storageRef, file).then((snapshot) => {
+          getDownloadURL(storageRef)
+            .then((url) => {
+              if (url) {
+                addDoc(
+                  collection(
+                    db,
+                    "storage",
+                    auth.currentUser.uid,
+                    currentFolder
+                  ),
+                  {
+                    fileName: file.name,
+                    fileURL: url,
+                    owner: auth.currentUser.uid,
+                    isFolder: false,
+                    createdAt: serverTimestamp(),
+                  }
+                );
+                handleClose();
+              }
+            })
+            .catch((error) => {
+              console.error("Error getting download URL:", error);
+            });
+        });
+      } else {
+        toast.error("There's no file!");
+      }
+    };
+
+    return (
+      <>
+        <Button className="mx-3" variant="primary " onClick={handleShow}>
+          <h6 className="fw-bold text-white px-3 mb-0 py-1">Upload File</h6>
+        </Button>{" "}
+        <Modal size="lg" show={show} onHide={handleClose}>
+          <Modal.Header className="bg-primary" closeButton>
+            <Modal.Title>File Name</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <input
+              onChange={(e) => setFile(e.target.files[0])}
+              type="file"
+              className="form-control"
+              placeholder="Folder Name"
+            />{" "}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={createFile}>Upload File</Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+
+  const downloadFile = (file) => {
+    console.log(file);
+    const fileUrl = file;
+    const link = document.createElement("a");
+    link.href = fileUrl;
+    link.target = "_blank";
+    link.download = "downloaded_file";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  function DropdownAction({ storage }) {
+    const downloadFIle = () => {
+      const fileUrl = message.fileUrl;
+      const link = document.createElement("a");
+      link.href = fileUrl;
+      link.target = "_blank";
+      link.download = "downloaded_file";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    };
+
+    const handleDelete = async () => {
+      const docMessage = doc(
+        db,
+        "storage",
+        auth.currentUser.uid,
+        "files",
+        storage.id
+      );
+      deleteDoc(docMessage).then(() => toast.success("Successfully Deleted!"));
+    };
+
+    return (
+      <Dropdown>
+        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+          <img src="./assets/images/pepicons-pencil_dots-y.png" alt="" />
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={handleDelete}>Delete</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  }
+
+  useEffect(() => {
+    if (auth.currentUser) {
+      fetchData();
+    }
+  }, []);
+
   return (
     <Layout>
-      <h1>Files</h1>
+      <div className="files-wrapper">
+        <div className="row">
+          <div className="col-lg-8">
+            <div className="wrapper">
+              <h2 className="fw-bold my-3 mx-2">
+                Files Storage
+                <FaFile className="mx-2" />
+              </h2>
+              <div
+                className="bg-info mx-2 mb-3"
+                style={{ width: "200px", height: "10px", borderRadius: 20 }}
+              ></div>
+            </div>
+          </div>
+          <div className="col-lg-4 flex">
+            <AddFolder />
+            <AddFile />
+          </div>
+          <div className="col-12 mx-3">
+            <Breadcrumb>
+              <Breadcrumb.Item onClick={fetchData}>Files</Breadcrumb.Item>
+
+              <Breadcrumb.Item active>
+                {currentFolder === "files" ? "" : currentFolder}
+              </Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+        </div>
+
+        <div className="row mt-5">
+          {storage &&
+            storages.map((storage) => {
+              return (
+                <>
+                  {storage.isFolder ? (
+                    <div className="col-6 col-md-4 col-lg-3 flex flex-column">
+                      <FaFolder
+                        color="gray"
+                        onClick={() => {
+                          setCurrentFolder(storage.name);
+                          fetchFolder(storage.name);
+                        }}
+                        size={70}
+                      />
+                      <div className="row">
+                        <div className="col-6 flex">
+                          <div>{storage.name}</div>
+                        </div>
+                        <div className="col-6">
+                          <DropdownAction storage={storage} />
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    <>
+                      <div
+                        key={storage.id}
+                        className="col-6 col-md-4 col-lg-3 flex flex-column"
+                      >
+                        <FaFile
+                          color="gray"
+                          onClick={() => downloadFile(storage.fileURL)}
+                          size={70}
+                        />
+                        <div className="row">
+                          <div className="col-lg-9 flex">
+                            <div className="text-nowrap">
+                              {" "}
+                              {storage.fileName}
+                            </div>
+                          </div>
+                          <div className="col-lg-3">
+                            <DropdownAction storage={storage} />
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </>
+              );
+            })}
+          {/* {storages &&
+            storages.map((storage) => {
+              return (
+                <>
+                  {storage.isFolder ? (
+                    <div
+                      key={storage.id}
+                      className="col-sm-3 flex flex-column"
+                      onClick={() => currentFolder(storage.name)}
+                    >
+                      <FaFolder size={70} />
+                      {storage.name}
+                    </div>
+                  ) : (
+                    <div
+                      key={storage.id}
+                      className="col-sm-3 flex flex-column"
+                      onClick={() => downloadFile(storage.fileURL)}
+                    >
+                      <FaFile size={70} />
+                      {storage.fileName}
+                    </div>
+                  )}
+                </>
+              );
+            })} */}
+        </div>
+      </div>
     </Layout>
   );
 };
--- src/pages/reports.jsx
+++ src/pages/reports.jsx
@@ -47,41 +47,6 @@ const messagesCollectionRef = collection(db, "messages");
 const incomingExternalRef = collection(db, "incoming-external");
 
 const currentDate = new Date();
-const options = {
-  // default is `save`
-  method: "open",
-  // default is Resolution.MEDIUM = 3, which should be enough, higher values
-  // increases the image quality but also the size of the PDF, so be careful
-  // using values higher than 10 when having multiple pages generated, it
-  // might cause the page to crash or hang.
-  resolution: Resolution.HIGH,
-  page: {
-    // margin is in MM, default is Margin.NONE = 0
-    margin: Margin.SMALL,
-    // default is 'A4'
-    format: "letter",
-    // default is 'portrait'
-    orientation: "landscape",
-  },
-  canvas: {
-    // default is 'image/jpeg' for better size performance
-    mimeType: "image/png",
-    qualityRatio: 1,
-  },
-  // Customize any value passed to the jsPDF instance and html2canvas
-  // function. You probably will not need this and things can break,
-  // so use with caution.
-  overrides: {
-    // see https://artskydj.github.io/jsPDF/docs/jsPDF.html for more options
-    pdf: {
-      compress: true,
-    },
-    // see https://html2canvas.hertzen.com/configuration for more options
-    canvas: {
-      useCORS: true,
-    },
-  },
-};
 
 const Reports = () => {
   const [users, setUsers] = useState([]);
@@ -92,8 +57,7 @@ const Reports = () => {
   const [currentFilter, setCurrentFilter] = useState(null);
 
   const { toPDF, targetRef } = usePDF({
-    filename: "reports.pdf",
-    options: options,
+    filename: "reports.pdf" + currentDate.toString(),
   });
 
   const fetchData = async () => {
