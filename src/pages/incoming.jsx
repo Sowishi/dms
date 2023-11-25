21b59824d4d693122087a94aa118fef41e2e59d3
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2023-11-25 19:05:54 +0800

    refactor routing

--- src/pages/incoming.jsx
+++ src/pages/incoming.jsx
@@ -45,6 +45,7 @@ import ViewModal from "../components/viewModal";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import PlaceHolder from "../components/placeholder";
 import moment from "moment";
+import Routing from "../components/routing";
 
 const userCollectionRef = collection(db, "users");
 const messagesCollectionRef = collection(db, "messages");
@@ -101,81 +102,6 @@ function UrgentModal(props) {
         <Button onClick={props.onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
-  );
-}
-function Routing(props) {
-  const { currentMessage } = props;
-  const [routing, setRouting] = useState();
-
-  const getRouting = (currentMessage) => {
-    const q = query(
-      collection(db, "routing", currentMessage.id, currentMessage.id),
-      orderBy("createdAt", "asc")
-    );
-    onSnapshot(q, (snapshot) => {
-      const output = [];
-      snapshot.docs.forEach((doc) => {
-        output.push({ ...doc.data(), id: doc.id });
-      });
-      setRouting(output);
-    });
-  };
-
-  useEffect(() => {
-    getRouting(currentMessage);
-  }, [currentMessage]);
-
-  return (
-    <>
-      <Offcanvas
-        placement="end"
-        show={props.showRouting}
-        onHide={props.handleCloseRouting}
-      >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Document Routing</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          {routing &&
-            routing.map((route) => {
-              return (
-                <div className="div">
-                  <div className="row">
-                    {route.createdAt && (
-                      <div className="col-5 py-4 d-flex justify-content-start alig-items-start">
-                        {moment(route.createdAt.toDate()).format("LLL")}
-                      </div>
-                    )}
-
-                    <div className="col-2 flex flex-column">
-                      <div
-                        className="div "
-                        style={{
-                          height: "100%",
-                          width: "5px",
-                          background: "gray",
-                        }}
-                      ></div>
-                      {route.status == "Recieved" && (
-                        <FaFile size={30} className="my-1" />
-                      )}
-                      {route.status == "Seen" && (
-                        <FaEye size={30} className="my-1" />
-                      )}
-                      {route.status == "Created" && (
-                        <FaCheck size={30} className="my-1" />
-                      )}
-                    </div>
-                    <div className="col-5 py-4 flex">
-                      <h5>{route.status}</h5>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-        </Offcanvas.Body>
-      </Offcanvas>
-    </>
   );
 }
 
--- src/pages/outgoing.jsx
+++ src/pages/outgoing.jsx
@@ -41,85 +41,9 @@ import ViewModal from "../components/viewModal";
 import PlaceHolder from "../components/placeholder";
 import moment from "moment";
 import axios from "axios";
+import Routing from "../components/routing";
 const userCollectionRef = collection(db, "users");
 const messagesCollectionRef = collection(db, "messages");
-
-function Routing(props) {
-  const { currentMessage } = props;
-  const [routing, setRouting] = useState();
-
-  const getRouting = (currentMessage) => {
-    const q = query(
-      collection(db, "routing", currentMessage.id, currentMessage.id),
-      orderBy("createdAt", "asc")
-    );
-    onSnapshot(q, (snapshot) => {
-      const output = [];
-      snapshot.docs.forEach((doc) => {
-        output.push({ ...doc.data(), id: doc.id });
-      });
-      setRouting(output);
-    });
-  };
-
-  useEffect(() => {
-    getRouting(currentMessage);
-  }, [currentMessage]);
-
-  console.log(routing);
-
-  return (
-    <>
-      <Offcanvas
-        placement="end"
-        show={props.showRouting}
-        onHide={props.handleCloseRouting}
-      >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Document Routing</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          {routing &&
-            routing.map((route) => {
-              return (
-                <div className="div">
-                  <div className="row">
-                    {route.createdAt && (
-                      <div className="col-5 py-4 d-flex justify-content-start alig-items-start">
-                        {moment(route.createdAt.toDate()).format("LLL")}
-                      </div>
-                    )}
-                    <div className="col-2 flex flex-column">
-                      <div
-                        className="div "
-                        style={{
-                          height: "100%",
-                          width: "5px",
-                          background: "gray",
-                        }}
-                      ></div>
-                      {route.status == "Recieved" && (
-                        <FaFile size={30} className="my-1" />
-                      )}
-                      {route.status == "Seen" && (
-                        <FaEye size={30} className="my-1" />
-                      )}
-                      {route.status == "Created" && (
-                        <FaCheck size={30} className="my-1" />
-                      )}
-                    </div>
-                    <div className="col-5 py-4 flex">
-                      <h5>{route.status}</h5>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-        </Offcanvas.Body>
-      </Offcanvas>
-    </>
-  );
-}
 
 const Outgoing = () => {
   const [modalShow, setModalShow] = useState(false);
