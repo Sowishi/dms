f8b34755132385474f4ebfc5732917542b7c2f21
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2023-11-25 19:46:29 +0800

    add design to routing

--- src/components/routing.jsx
+++ src/components/routing.jsx
@@ -2,8 +2,16 @@ import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../../firebase";
 import { Offcanvas } from "react-bootstrap";
-import { FaCheck, FaEye, FaFile } from "react-icons/fa";
+import {
+  FaCheck,
+  FaCheckCircle,
+  FaCross,
+  FaExclamationCircle,
+  FaEye,
+  FaFile,
+} from "react-icons/fa";
 import moment from "moment";
+import BounceLoader from "react-spinners/BounceLoader";
 
 const Routing = (props) => {
   const { currentMessage } = props;
@@ -27,6 +35,12 @@ const Routing = (props) => {
     getRouting(currentMessage);
   }, [currentMessage]);
 
+  let lastItem = 0;
+
+  if (routing) {
+    lastItem = routing.length - 1;
+  }
+
   return (
     <>
       <Offcanvas
@@ -34,42 +48,85 @@ const Routing = (props) => {
         show={props.showRouting}
         onHide={props.handleCloseRouting}
       >
-        <Offcanvas.Header closeButton>
+        <Offcanvas.Header className="bg-primary text-white" closeButton>
           <Offcanvas.Title>Document Routing</Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body>
+        <Offcanvas.Body className="m-3">
           {routing &&
-            routing.map((route) => {
+            routing.map((route, index) => {
               return (
                 <div className="div">
                   <div className="row">
                     {route.createdAt && (
-                      <div className="col-5 py-4 d-flex justify-content-start alig-items-start">
+                      <div
+                        style={{ opacity: lastItem == index ? 1 : 0.6 }}
+                        className="col-5 py-4 d-flex justify-content-start alig-items-start"
+                      >
                         {moment(route.createdAt.toDate()).format("LLL")}
                       </div>
                     )}
 
-                    <div className="col-2 flex flex-column">
+                    <div
+                      style={{ opacity: lastItem == index ? 1 : 0.6 }}
+                      className="col-2 flex flex-column"
+                    >
                       <div
                         className="div "
                         style={{
                           height: "100%",
-                          width: "5px",
+                          width: "2px",
                           background: "gray",
                         }}
                       ></div>
                       {route.status == "Recieved" && (
-                        <FaFile size={30} className="my-1" />
+                        <FaFile color="green" size={30} className="my-1" />
                       )}
                       {route.status == "Seen" && (
                         <FaEye size={30} className="my-1" />
                       )}
                       {route.status == "Created" && (
-                        <FaCheck size={30} className="my-1" />
+                        <FaCheckCircle size={30} className="my-1" />
                       )}
+                      {route.status == "Rejected" && (
+                        <FaExclamationCircle
+                          color="red"
+                          size={30}
+                          className="my-1"
+                        />
+                      )}{" "}
                     </div>
-                    <div className="col-5 py-4 flex">
-                      <h5>{route.status}</h5>
+                    <div
+                      style={{ opacity: lastItem == index ? 1 : 0.6 }}
+                      className="col-5 py-4 text-left"
+                    >
+                      <h5
+                        className={`fw-bold ${
+                          route.status == "Recieved" ? "text-primary" : ""
+                        }  ${route.status == "Rejected" ? "text-danger" : ""}`}
+                      >
+                        {route.status}
+                      </h5>
+                      {route.status == "Created" && (
+                        <p style={{ fontSize: "13px", fontStyle: "italic" }}>
+                          your document has been created
+                        </p>
+                      )}
+                      {route.status == "Seen" && (
+                        <p style={{ fontSize: "13px", fontStyle: "italic" }}>
+                          your document has been seen by the reciever
+                        </p>
+                      )}
+                      {route.status == "Recieved" && (
+                        <p style={{ fontSize: "13px", fontStyle: "italic" }}>
+                          your document has been seen successfully recieved by
+                          the reciever
+                        </p>
+                      )}
+                      {route.status == "Rejected" && (
+                        <p style={{ fontSize: "13px", fontStyle: "italic" }}>
+                          your document has been seen rejected by the reciever
+                        </p>
+                      )}
                     </div>
                   </div>
                 </div>
